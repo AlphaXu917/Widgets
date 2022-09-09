3d1837e9f7fa3582484a9f9c84fcc9d6f3804f82
Alpha <646266750@qq.com>
2022-09-09 14:05:32 +0800

    add kotlin-android-extensions

--- app/src/main/java/com/alpha/myviews/MainActivity.kt
+++ app/src/main/java/com/alpha/myviews/MainActivity.kt
@@ -5,13 +5,13 @@ import android.view.View
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.alpha.views.FloatView
-import com.alpha.views.StepView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StepView>(R.id.mStepView).setOnClickListener {
+        mStepView.setOnClickListener {
             val contentView = this.window.decorView.findViewById<View>(android.R.id.content) as FrameLayout
             contentView.addView(FloatView(this))
         }
